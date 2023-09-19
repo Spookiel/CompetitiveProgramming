011c76a6495e0feca603d2b0bc9130b316d5d7f0
Luke M <luke.moran2004@gmail.com>
2023-09-19 01:52:44 +0100

    Added BAPC19 - B

---
+++ BAPC/2019/B.cpp
@@ -0,0 +1,18 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+#define INF 1e12
+int main(){
+
+    int n;
+    cin >> n;
+
+    if(n%2 == 0){
+        cout << "Alice\n1\n";
+    }else{
+        cout << "Bob\n";
+    }
+    return 0;
+}
